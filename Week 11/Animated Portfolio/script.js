50620d17298156b7df8651f7aac842b6d5b6c2c2
Mahak Patel <mahakpateluiux@gmail.com>
2025-04-22 02:36:32 -0400

    Much better version

---
+++ Week 11/Animated Portfolio/script.js
@@ -0,0 +1,14 @@
+/**
+ * HTTP5121 CSS Animation Project
+ * Weather-Themed Interactive Portfolio
+ * 
+ * This script handles:
+ * - Theme switching functionality
+ * - Weather effect generation (snow, rain, stars)
+ * - Mobile menu toggle
+ * - Company carousel for mobile
+ * - Form submission animation
+ * 
+ * All visual animations are handled by CSS, this script only triggers them
+ */
+
